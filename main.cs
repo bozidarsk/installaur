5d0e11b2340e1f7d86c7ededfebfe8ad8322eb8c
bozidarsk <bozidarkabahcijski@gmail.com>
2023-09-04 21:33:54 +0300

    Fixed bugs.

--- main.cs
+++ main.cs
@@ -7,74 +7,73 @@ using System.IO;
 
 public static class Program 
 {
-	private static void zsh(string command) => zsh(command, out int code);
-	private static void zsh(string command, out int code) 
+	public static string Shell(string command, bool getStdOut = false) => Shell(command, out int code, getStdOut);
+	public static string Shell(string command, out int code, bool getStdOut = false) 
 	{
 		ProcessStartInfo info = new ProcessStartInfo();
-		info.FileName = "/bin/zsh";
-		info.Arguments = "-c \"" + command + "\"";
+		info.FileName = Environment.GetEnvironmentVariable("SHELL");
+		info.Arguments = $"-c \"{command}\"";
 		info.UseShellExecute = false;
+		info.RedirectStandardOutput = getStdOut;
+		info.RedirectStandardError = false;
 
 		Process proc = new Process();
 		proc.StartInfo = info;
 		proc.Start();
 		proc.WaitForExit();
 		code = proc.ExitCode;
+
+		return getStdOut ? proc.StandardOutput.ReadToEnd() : null;
 	}
 
 	private static void InstallAUR(params string[] packages) 
 	{
 		foreach (string package in packages) 
 		{
-			zsh($"git clone 'https://aur.archlinux.org/{package}.git' '{Config.DownloadDir}/{package}'");
-			Directory.SetCurrentDirectory($"{Config.DownloadDir}/{package}");
+			string path = $"{Config.DownloadDir}/{package}";
+
+			Shell($"git clone 'https://aur.archlinux.org/{package}.git' '{path}'");
+			if (File.Exists("PKGBUILD")) { Console.WriteLine($"Pacakge '{package}' is empty."); continue; }
 
 			string dependencies = (!Config.NoBuildtime ? "$makedepends " : "") + (!Config.NoRuntime ? "$depends" : "");
-			zsh($"source '{Config.DownloadDir}/{package}/PKGBUILD'; echo {dependencies} | tr -d '\\n' > '/tmp/installaur-{package}'");
-
-			foreach (string dependency in File.ReadAllText($"/tmp/installaur-{package}").Split(' ')) 
+			foreach (string dependency in Shell($"source '{path}/PKGBUILD'; echo {dependencies} | tr -d '\\n'", true).Split(' ')) 
 			{
 				if (string.IsNullOrEmpty(dependency)) { continue; }
 				int code;
 
-				zsh($"pacman -Q '{dependency}'", out code);
+				Shell($"pacman -Q '{dependency}'", out code);
 				if (code == 1) 
 				{
-					zsh($"sudo pacman -Sy --noconfirm '{dependency}'", out code);
-					if (code == 1) { InstallAUR(dependency); }
+					Shell($"sudo pacman -Sy --noconfirm '{dependency}'", out code);
+					if (code == 1) 
+					{
+						InstallAUR(dependency);
+						Directory.SetCurrentDirectory(path);
+					}
 				}
 			}
 
 			if (Config.DependenciesOnly) { continue; }
+			Directory.SetCurrentDirectory(path);
 
 			string makepkg = $"makepkg{!Config.PackageOnly ? " -i" : ""}";
-			zsh($"script -qe -c '{makepkg}' '/tmp/installaur-{package}'");
+			Shell($"script -qe -c '{makepkg}' '/tmp/installaur-{package}'");
 
-			string[] keys = 
-				File.ReadAllText($"/tmp/installaur-{package}")
+			IEnumerable<string> keys = File.ReadAllText($"/tmp/installaur-{package}")
 				.Split('\n')
 				.Where(x => x.Contains("unknown public key"))
 				.Select(x => x.Remove(0, x.IndexOf("unknown public key") + "unknown public key".Length + 1).Substring(0, 16))
-				.ToArray()
 			;
 
-			if (keys.Length > 0) 
-			{
-				string allkeys = "";
-				for (int i = 0; i < keys.Length; i++) { allkeys += keys[i] + " "; }
-				zsh($"gpg --recv-keys {allkeys}");
-				zsh(makepkg);
-			}
+			foreach (string key in keys) { Shell($"gpg --recv-keys {key}"); }
+			if (keys.Any()) { Shell(makepkg); }
 
-			if (!Config.PackageOnly) { zsh($"rm -rf '{Config.DownloadDir}/{package}'"); }
-			zsh($"rm '/tmp/installaur-{package}'");
+			File.Delete($"/tmp/installaur-{package}");
 		}
 	}
 
 	private static int Main(string[] args) 
 	{
-		if (!File.Exists("/bin/zsh")) { Console.WriteLine("zsh is required."); return 1; }
-
 		if (args.Length == 0 || (args.Length == 1 && (args[0] == "-h" || args[0] == "--help" || args[0] == "help"))) 
 		{
 			Console.WriteLine("Usage:\n\tinstallaur <package0> [package1] [package...] [options]");
